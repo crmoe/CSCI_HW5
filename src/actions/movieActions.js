d904b54006171f3bdd39614cca1f5093caa2434e
crmoe <60358861+crmoe@users.noreply.github.com>
2020-05-01 18:24:59 -0600

    Add files via upload

---
+++ src/actions/movieActions.js
@@ -0,0 +1,97 @@
+import actionTypes from '../constants/actionTypes';
+import runtimeEnv from '@mars/heroku-js-runtime-env';
+
+function moviesFetched(movies) {
+    return {
+        type: actionTypes.FETCH_MOVIES,
+        movies: movies
+    }
+}
+
+function movieFetched(movie) {
+    return {
+        type: actionTypes.FETCH_MOVIE,
+        selectedMovie: movie
+    }
+}
+
+function movieSet(movie) {
+    return {
+        type: actionTypes.SET_MOVIE,
+        selectedMovie: movie
+    }
+}
+
+function reviewSet(review) {
+    return {
+        type: actionTypes.SET_REVIEW,
+        selectedReview: review
+    }
+}
+
+function reviewsFetched(reviews) {
+    return {
+        type: actionTypes.FETCH_REVIEWS,
+        selectedReview: reviews
+    }
+}
+
+export function setMovie(movie) {
+    return dispatch => {
+        dispatch(movieSet(movie))
+    }
+}
+
+export function setReview(review) {
+    return dispatch => {
+        dispatch(reviewSet(review))
+    }
+}
+
+export function fetchMovies() {
+    const env = runtimeEnv();
+    return dispatch => {
+        return fetch(`${env.REACT_APP_API_URL}/movies?reviews=false`, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': localStorage.getItem('token')
+            },
+            mode: 'cors'})
+            .then( (response) => {
+                if (!response.ok) {
+                    throw Error(response.statusText);
+                }
+                return response.json();
+            })
+            .then( (res) => {
+                dispatch(moviesFetched(res.movie));
+            })
+            .catch( (e) => console.log(e) );
+    }
+}
+
+export function fetchMovie(movieId){
+    const env = runtimeEnv();
+    return dispatch => {
+        return fetch(`${env.REACT_APP_API_URL}/movies/${movieId}?reviews=true`, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': localStorage.getItem('token')
+            },
+            mode: 'cors'})
+            .then( (response) => {
+                if (!response.ok) {
+                    throw Error(response.statusText);
+                }
+                return response.json();
+            })
+            .then( (res) => {
+                dispatch(movieFetched(res.movie));
+            })
+            .catch( (e) => console.log(e) );
+    }
+}
---
+++ src/reducers/movieReducer.js
@@ -0,0 +1,32 @@
+import constants from '../constants/actionTypes'
+
+var initialState = {
+    movies: [],
+    selectedMovie: null
+}
+
+export default (state = initialState, action) => {
+    var updated = Object.assign({}, state);
+
+    switch(action.type) {
+        case constants.FETCH_MOVIES:
+            updated['movies'] = action.movies;
+            updated['selectedMovie'] = action.movies[0];
+            return updated;
+        case constants.SET_MOVIE:
+            updated['selectedMovie'] = action.selectedMovie;
+            return updated;
+        case constants.FETCH_MOVIE:
+            updated['selectedMovie'] = action.selectedMovie;
+            return updated;
+        case constants.SET_REVIEW:
+            updated['selectedReview'] = action.selectedReview;
+            return updated;
+        case constants.FETCH_REVIEWS:
+            updated['reviews'] = action.reviews;
+            updated['selectedReview'] = action.reviews[0];
+            return updated;
+        default:
+            return state;
+    }
+}
